455b24e6f7fed135c2bb5c278979c17c02e21b73
Sravya-Nammi <131783900+Sravya-Nammi@users.noreply.github.com>
2024-02-20 12:01:33 +0100

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+title = st.text_input('Name', ' ')
+
+st.button("Mark Attendance", type="primary")
+if st.button('Click Attendance'):
+    st.write('Attendance registered')
+else:
+    st.write('Not registered')
